f4f309589d654fefbb67f35fdfa5bdab2ae2f9b5
Deepaksharma106 <deepak.salwan13@gmail.com>
2021-02-14 21:35:24 +0530

    first blood

---
+++ src/main/java/com/test/HelloWorldSpringBoot/HelloWorldSpringBootApplication.java
@@ -0,0 +1,29 @@
+package com.test.HelloWorldSpringBoot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloWorldSpringBootApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringBootApplication.class, args);
+	}
+
+	@RestController
+	public static class GreetingController {
+
+		@Value("${example.property}")
+		private String test;
+
+		@GetMapping(value = "/greet")
+		public String greet(){
+			System.out.println("this is testing property from application properties file "+test);
+			return test;
+		}
+	}
+}
